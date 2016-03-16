4701c73086cd7e4cac153c137e93045df20f1a13
Jeremy Jones <Jeremy Jones>
2016-03-15 21:01:07 -0700

    JSLint clean up, home git test

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
+/*jslint node: true */
+"use strict";
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req,res) {
+app.get('/', function (req, res) {
    //res.send('<h1>Hello World</h1>');
+
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection',function(socket){
+io.on('connection', function (socket) {
     console.log('a user connected');
-    socket.on('disconnect',function(){
+    socket.on('disconnect', function () {
         console.log('user disconnected');
     });
 
-    socket.on('chat message', function(msg){
-        console.log('Message: ' + msg)
+    socket.on('chat message', function (msg) {
+        console.log('Message: ' + msg);
     });
+
+
+    socket.on('chat typing', function () {
+        console.log('user is typing...');
+    });
+
 });
 
 
-http.listen(3000,function(){
+http.listen(3000, function () {
     console.log('listening on 3000');
 });
